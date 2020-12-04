4fa0f768812dfc3dd5fcef7b00ec37b8c4cdfcd7
aratter <arataster@gmail.com>
2020-12-05 01:13:57 +0900

    fix anchor

--- assessment.js
+++ assessment.js
@@ -38,9 +38,9 @@
     const anchor = document.createElement('a');
     const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%A8%E3%81%93%E3%82%8D&text=';
     + encodeURIComponent(result);
-    anchorSetAttribute('href',hrefValue);
-    anchorClassName = 'twitter-hashtag-button';
-    anchorInnerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%A8%E3%81%93%E3%82%8D';
+    anchor.setAttribute('href',hrefValue);
+    anchor.className = 'twitter-hashtag-button';
+    anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%A8%E3%81%93%E3%82%8D';
     tweetDivided.appendChild(anchor);
     twttr.widgets.load();
    };
